14406727a85468ea59b8330401a6fe4e4305ee71
Dipak Patil <123900322+dipakpatil2001@users.noreply.github.com>
2024-10-02 18:21:50 +0530

    Delete ProjectProfile directory

--- ProjectProfile/js/animations.js
+++
@@ -1,6 +0,0 @@
-window.onload = function() {
-    const elements = document.querySelectorAll('.slide-in, .fade-in');
-    elements.forEach((element) => {
-        element.classList.add('visible');
-    });
-};
